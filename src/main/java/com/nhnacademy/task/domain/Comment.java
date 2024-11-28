41e794c0340b84fee60a487d904fbcbbf727e363
yunjiho <yoonwlgh12@gmail.com>
2024-11-28 14:31:15 +0900

    comment 도메인 매핑 (관계 제외) 완료 했습니다

--- src/main/java/com/nhnacademy/task/domain/Comment.java
+++ src/main/java/com/nhnacademy/task/domain/Comment.java
@@ -1,8 +1,29 @@
 package com.nhnacademy.task.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private long commentId;
+
+    @Column(name = "task_id")
+    private long taskId;
+
+    @Column(name = "wirter_id", nullable = false, length = 50)
+    private String writerId;
+
+    @Column(length = 255, nullable = false)
+    private String content;
 
 }
