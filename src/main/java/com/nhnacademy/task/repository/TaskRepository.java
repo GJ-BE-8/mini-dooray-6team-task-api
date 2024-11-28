1ea7be6039378e601333c9491aa392bddc467277
yunjiho <yoonwlgh12@gmail.com>
2024-11-28 16:06:15 +0900

    TaskService 완료!

--- src/main/java/com/nhnacademy/task/repository/TaskRepository.java
+++ src/main/java/com/nhnacademy/task/repository/TaskRepository.java
@@ -3,5 +3,9 @@ package com.nhnacademy.task.repository;
 import com.nhnacademy.task.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    //특정 프로젝트에 해당하는 task 조회
+    List<Task> findByProjectId(long projectId);
 }
--- src/main/java/com/nhnacademy/task/service/TaskService.java
+++ src/main/java/com/nhnacademy/task/service/TaskService.java
@@ -1,4 +1,21 @@
 package com.nhnacademy.task.service;
 
+import com.nhnacademy.task.domain.Task;
+
+import java.util.List;
+
 public interface TaskService {
+
+    //새 task 등록
+    Task createTask(long projectId, long milestoneId, String content);
+
+    //task 수정
+    Task updateTask(Long taskId, String updatedContent, Long milestoneId); // 태스크 수정
+
+
+    //task 삭제
+    void deleteTask(long taskId);
+
+    //특정 프로젝트의 task 목록 조회
+    List<Task> getTasksByProjectId(long projectId);
 }
