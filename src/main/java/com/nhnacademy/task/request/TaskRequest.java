230d6d993c821c667c637a924e37537ea8c9ae71
이동수 <dongsu6666@naver.com>
2024-11-28 20:02:52 +0900

    TaskRequest 생성

---
+++ src/main/java/com/nhnacademy/task/request/TaskRequest.java
@@ -0,0 +1,23 @@
+package com.nhnacademy.task.request;
+
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TaskRequest {
+    @NotNull
+    private long projectId;
+    @NotNull
+    @Length(min = 1, max = 300)
+    private String content;
+
+    private long mileStoneId;
+}
