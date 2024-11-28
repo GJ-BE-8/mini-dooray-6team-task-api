cd04eb5fbf871c0b870efe880a3aee216cc136d3
이동수 <dongsu6666@naver.com>
2024-11-28 19:56:03 +0900

    Project, Comment 양방향

--- src/main/java/com/nhnacademy/task/Controller/TaskController.java
+++ src/main/java/com/nhnacademy/task/Controller/TaskController.java
@@ -1,9 +1,22 @@
 package com.nhnacademy.task.Controller;
 
+import com.nhnacademy.task.domain.Task;
+import com.nhnacademy.task.service.TaskService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class TaskController {
+    private final TaskService taskService;
+
+    @PostMapping("/task")
+    public Task createTask(@RequestBody Task task) {
+        long projectId = task.getProject().getProjectId();
+        long mileStoneId = task.getMileStone().getMilestoneId();
+        return taskService.saveTask(projectId, mileStoneId, task.getContent()   );
+    }
+
 }
