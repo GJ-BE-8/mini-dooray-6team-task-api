b258471b33ce88fecc133b337f52b41f037b3af2
yunjiho <yoonwlgh12@gmail.com>
2024-11-28 14:12:59 +0900

    validation 의존성 추가, task 진행중

--- src/main/java/com/nhnacademy/task/domain/Task.java
+++ src/main/java/com/nhnacademy/task/domain/Task.java
@@ -1,7 +1,28 @@
 package com.nhnacademy.task.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long taskId;
+
+    private long projectId;
+
+    private long milestoneId;
+
+    private String content;
+
 }
