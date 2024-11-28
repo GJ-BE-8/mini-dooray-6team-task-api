5911bd40f832b4f8a9d0e754d121e9c6926c6b8b
yunjiho <yoonwlgh12@gmail.com>
2024-11-28 16:59:24 +0900

    projectService 완료!

--- src/main/java/com/nhnacademy/task/service/Impl/ProjectServiceImpl.java
+++ src/main/java/com/nhnacademy/task/service/Impl/ProjectServiceImpl.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.task.service.Impl;
 
+import com.nhnacademy.task.domain.Project;
 import com.nhnacademy.task.repository.ProjectRepository;
 import com.nhnacademy.task.service.ProjectService;
 import lombok.RequiredArgsConstructor;
@@ -10,4 +11,13 @@ import org.springframework.stereotype.Service;
 public class ProjectServiceImpl implements ProjectService {
 
     private final ProjectRepository projectRepository;
+
+    @Override
+    public Project saveProject(String projectName, String status, long adminId) {
+        Project project = new Project();
+        project.setProjectName(projectName);
+        project.setStatus(status);
+        project.setAdminId(adminId);
+        return projectRepository.save(project);
+    }
 }
--- src/main/java/com/nhnacademy/task/service/ProjectService.java
+++ src/main/java/com/nhnacademy/task/service/ProjectService.java
@@ -7,10 +7,6 @@ public interface ProjectService {
     //새 프로젝트 생성 - post로 create command가 들어올 건데 게이트 웨이랑 맞춰야함
     Project saveProject(String projectName, String status, long adminId);
 
-    //프로젝트에 멤버 등록
-    //특정 유저가 속한 프로젝트 목록 조회
-    //==>
-
     //프로젝트에 마일스톤 추가
     //프로젝트에 태그 추가
     //태그 삭제(task에서도 삭제됨)
