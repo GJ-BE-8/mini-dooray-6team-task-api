5b1e25057724e3a82550755c6073e8c8c8ecf754
이동수 <dongsu6666@naver.com>
2024-11-28 14:23:27 +0900

    Tag 도메인 추가

--- src/main/java/com/nhnacademy/task/domain/Tag.java
+++ src/main/java/com/nhnacademy/task/domain/Tag.java
@@ -1,7 +1,21 @@
 package com.nhnacademy.task.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tag_id", nullable = false)
+    private long tagId;
+
+    @Column(name = "project_id", nullable = false)
+    private String projectId;
 }
