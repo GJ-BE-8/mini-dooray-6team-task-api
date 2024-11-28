d9bfe0fb38f447a3e90394d73f81db202bffe522
yunjiho <yoonwlgh12@gmail.com>
2024-11-28 15:39:25 +0900

    Service에 필요한 인터페이스를 생성했습니다

---
+++ src/main/java/com/nhnacademy/task/service/CommentService.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.task.service;
+
+import com.nhnacademy.task.domain.Comment;
+
+public interface CommentService {
+    //새 코멘트 등록
+    Comment saveComment(long taskId, String writerId, String content);
+
+    //코멘트 수정
+    Comment updateComment(long commentId, String updateContent);
+
+    //코멘트 삭제
+    void deleteComment(long commentId);
+}
