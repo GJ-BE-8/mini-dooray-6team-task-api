fad92f8567c958e2c99cbab03a701996c45fe826
yunjiho <yoonwlgh12@gmail.com>
2024-11-30 15:55:56 +0900

    commit test 진행중입니다

---
+++ src/test/java/com/nhnacademy/task/repository/CommentRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.nhnacademy.task.repository;
+
+import com.nhnacademy.task.domain.Comment;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class CommentRepositoryTest {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Sql("comment-test.sql")
+    @Test
+    void testFindAllComments(){
+        List<Comment> comments = commentRepository.findAll();
+
+        assertThat(comments).hasSize(1);
+    }
+
+}
