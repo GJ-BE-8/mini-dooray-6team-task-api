011275ca640885cc5bdb17e762ecbafab9cffb4b
이동수 <dongsu6666@naver.com>
2024-11-30 13:53:29 +0900

    addMilestoneToProject -> 파라미터 String milestoneName을 MileStone milestone으로 바꿈

--- src/main/java/com/nhnacademy/task/service/MileStoneService.java
+++ src/main/java/com/nhnacademy/task/service/MileStoneService.java
@@ -3,7 +3,7 @@ package com.nhnacademy.task.service;
 import com.nhnacademy.task.domain.MileStone;
 
 public interface MileStoneService {
-    MileStone addMilestoneToProject(Long projectId, String milestoneName);
+    MileStone addMilestoneToProject(Long projectId, MileStone milestone);
     //mileStone 반환 타입 몰라서 일단 String으로 진행
 
     void deleteMileStone(long milestoneId);
