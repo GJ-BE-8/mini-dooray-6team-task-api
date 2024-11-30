7a77e08f39af21c7fefe6d7c5cb0d458502bcf6b
이동수 <dongsu6666@naver.com>
2024-11-30 13:41:00 +0900

    ProjectController - save만 구현, 아직 미완성임

--- src/main/java/com/nhnacademy/task/Controller/ProjectController.java
+++ src/main/java/com/nhnacademy/task/Controller/ProjectController.java
@@ -1,9 +1,21 @@
 package com.nhnacademy.task.Controller;
 
+import com.nhnacademy.task.domain.Project;
+import com.nhnacademy.task.request.ProjectRequest;
+import com.nhnacademy.task.service.ProjectService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ProjectController {
+    private final ProjectService projectService;
+
+    @PostMapping("/project")
+    public Project createProject(@RequestBody ProjectRequest projectRequest) {
+        Project addProject = projectService.saveProject(projectRequest.getProjectName(), projectRequest.getStatus(), projectRequest.getAdminId());
+        return addProject;
+    }
 }
