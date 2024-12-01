bf2704e61eae9a15073735bcd211aae14f58b959
이동수 <dongsu6666@naver.com>
2024-12-01 16:16:25 +0900

    projectControllerTest 완료

--- src/test/java/com/nhnacademy/task/repository/ProjectRepositoryTest.java
+++ src/test/java/com/nhnacademy/task/repository/ProjectRepositoryTest.java
@@ -1,8 +1,11 @@
 package com.nhnacademy.task.repository;
 
 import com.nhnacademy.task.domain.Project;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
